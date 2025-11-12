fa8ee8f56a67ecc175bc6c34ca6d5908b851976d
maxx-maxxing <maxxlazos93@gmail.com>
2025-11-12 07:33:48 -0700

    finished updates

--- Assets/Balloon Popper/ScoreManager.cs
+++ Assets/Balloon Popper/ScoreManager.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    private int score;
+    private int score = 0;
+    [SerializeField]
+    private TMP_Text scoreText;
+
+    void Start()
+    {
+        UpdateScore();
+    }
 
     public void IncreaseScore(int amount)
     {
         score += amount;
+        UpdateScore();
+    }
+
+    public void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
     }
 }
